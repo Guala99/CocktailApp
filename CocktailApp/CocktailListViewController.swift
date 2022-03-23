c216a601996ae4a6e8edbe09d3f459587d19534f
Guala <gualandris99@gmail.com>
2022-03-23 23:22:07 +0100

    Setting up of tableView

---
+++ CocktailApp/CocktailListViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  CocktailApp
+//
+//  Created by Andrea Gualandris on 23/03/2022.
+//
+
+import UIKit
+
+class CocktailListViewController: UIViewController {
+    
+    var cocktailsModel: [String] = ["Margarita", "Mojito", "Tequila"]
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cocktailID")
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpUI()
+        makeFetchingRequest()
+    }
+    
+    func setUpUI(){
+        
+        self.view.addSubview(tableView)
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    }
+
+    
+    func makeFetchingRequest() {
+        
+    }
+
+}
+
+extension CocktailListViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cocktailsModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cocktailID")
+        cell.textLabel?.text = cocktailsModel[indexPath.item]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Choose what you want to drink"
+    }
+}
+
